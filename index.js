938c6b5fdfee4211367047d45fa89073327788ed
Justin Chittarath <justinchitt@gmail.com>
2021-09-04 17:35:56 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const { tsNamespaceExportDeclaration } = require("@babel/types");
+
 // Code your solutions in this file
+
+function writeCards(name, eventName) {
+    let arr = [];
+    for (let i = 0; i < name.length; i++) {
+         arr.push(`Thank you, ${name[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arr;
+}
+function countDown(number) {
+    let countDown = number;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
